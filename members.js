db6387254eca3a252204af3a49db72511fdb01d1
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2022-02-22 16:02:54 +0000

    Update members.js

--- members.js
+++ members.js
@@ -1,3 +1,11 @@
+//placeholder data:
+/*
+first name ,last name ,job title         ,department  ,manager  ,location  ,first aider ,user slack ,timezone
+John       ,Doe       ,Software Engineer ,Engineering ,null     ,Glasgow   ,No          ,johndoe    ,America/Los_Angeles
+Jane       ,Doe       ,Software Engineer ,Engineering ,John Doe ,Leeds     ,No          ,janedoe    ,America/Los_Angeles
+Bob        ,Doe       ,Software Engineer ,Engineering ,Jane Doe ,Cape Town ,No          ,bobdoe     ,America/Los_Angeles
+Sally      ,Doe       ,Software Engineer ,Engineering ,Jane Doe ,Rome      ,No          ,sallydoe   ,America/Los_Angeles
+*/
 const members = [];
 
 module.exports = members;
